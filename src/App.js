9878639c6723178d8443c1ca325d3fff6a28e91a
Dragos <corneadragos89@gmail.com>
2022-02-25 16:44:43 +0200

    AddingComponents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from './components/Todo'
 function App() {
-  return <div> Hello !</div>;
+
+  return ( // looks like html but in behind it's standard js
+    <div>
+      <h1>My Todos</h1>
+      <Todo text ='learn'/>
+      <Todo text = 'master'/>
+      <Todo text = 'explore'/>
+    </div>
+  );
 }
 
 export default App;
