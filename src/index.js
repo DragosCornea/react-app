e6a2082d31046209d3da78ff869c266e39b9a58e
Dragos <corneadragos89@gmail.com>
2022-02-25 15:59:22 +0200

    InitialCommit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // import ReactDom object from library
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />,  document.getElementById('root'));// render met, parse html code , tell react where should be render in DOM
